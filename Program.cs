04f29e89911171b7495dff76da0aea6293db586f
Pang <narumonokanoto@gmail.com>
2023-05-20 23:17:26 +0700

    5

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
 ﻿class Program
 {
-  static int Calculate(int a, int b)
-  {
-    if (a > b)
-    {
-      return a;
-    }
-    return b;
-  }
 
     static void Main(string[] args)
     {
+        Console.Write("Input Size: ");
         int size = int.Parse(Console.ReadLine());
+        Console.Write("Input Number: ");
+        int num = int.Parse(Console.ReadLine());
         int[] road = new int[size];
-
-        
+        Input(ref road);
         Console.WriteLine("{0}",road[size]);
   }
         
+
+static void Input(ref int[] road)
+  {
+    for (int i = 0; i < road.Length; i++)
+    {
+      road[i] = int.Parse(Console.ReadLine());
+    }
+  }
 }
