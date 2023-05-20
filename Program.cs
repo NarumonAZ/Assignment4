a8528f51d126f22c117a8c82aa6b930b241eca0c
Pang <narumonokanoto@gmail.com>
2023-05-20 22:59:18 +0700

    4

--- Program.cs
+++ Program.cs
@@ -1,29 +1,21 @@
-﻿class Program {
-        static void Swap(ref float a, ref float b) {
-        float tmp = a;
-        a = b;
-        b = tmp;
+﻿class Program
+{
+  static int Calculate(int a, int b)
+  {
+    if (a > b)
+    {
+      return a;
     }
-    static void SwapIfHigher(ref float a, ref float b) {
-        if (a < b) {
-            Swap(ref a, ref b);
-        }
-    }
+    return b;
+  }
 
-static void Main(string[] args) {
-       int size = int.Parse(Console.ReadLine());
-       float[] num = new float[size];
-       for (int i = 0; i < size; i++) {
-        num[i] = float.Parse(Console.ReadLine());
-       }
-       // sort
-       for(int i = size - 1; i > 0; i--) {
-           for(int j = 0; j < i; j++) {
-            SwapIfHigher(ref num[j], ref num[j+1]);
-           }
-       }
-       for (int i = 0; i < size; i++) {
-          Console.Write("{0} ", num[i]);
-       }
-    }
+    static void Main(string[] args)
+    {
+        int size = int.Parse(Console.ReadLine());
+        int[] road = new int[size];
+
+        
+        Console.WriteLine("{0}",road[size]);
+  }
+        
 }
